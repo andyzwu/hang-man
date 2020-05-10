e04b191bb85cee015ec7128a9b3e2c7f27f96c98
andyzwu <65090510+andyzwu@users.noreply.github.com>
2020-05-09 21:57:47 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,54 @@
 console.log('the script is included');
 
 var app = {
-  
   wordList: [
     'helicopter',
-    'playstation'  
+    'playstation',  
+    'watermelon',
+    'textbook'
   ],
   currentWord: ''
 };
 
+function randomNumber() {
+  let number = Math.floor(Math.random() * app.wordList.length);
+
+  return number;
+}
+
 function startGame() {
+
+  let index = randomNumber();
+
+  console.log(index);
   
-  app.currentWord = app.wordList[0];
+  app.currentWord = app.wordList[index];
   
   console.log('the word is: ', app.currentWord);
   
   //TO DO: output the word on screen
 
+}
+
+/* Letter logic: Loop that checks all letters in 'currentWord.length' 
+if they match 'inputLetter', else then 'livesLeft' = 'livesLeft - 1'
+loop breaks if any letter matches 'input letter', and respective blank(s) filled
+*/
+
+// Endgame logic:
+function winLose(currentLives, wordStatus) {
+  // Lose logic
+  if (currentLives === 0) {
+    console.log('Game over!');
+    // Ends the game
+  } else {
+    // Continues the game
+  };
+  // Win logic
+  if (wordStatus === true /* true is a placeholder for the correct status of the word */) {
+    console.log('You win!');
+    // Ends the game
+  } else {
+    // Continues the game
+  }
 }
